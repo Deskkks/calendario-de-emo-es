936adb88f936ff6aeffaf5a11da04c1c86b408ea
Kayky <109158144+Deskkks@users.noreply.github.com>
2022-10-23 17:50:36 -0300

    cor no calendario

--- scpripts/main.js
+++ scpripts/main.js
@@ -1,18 +1,17 @@
 const mesBr = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 const dias = document.querySelector('#dias')
+const divsDia = dias.getElementsByTagName('div')
+
 
 function colocarDias(mes, ano) {
   document.querySelector('span#mes').innerHTML = mesBr[mes]
   document.querySelector('span#ano').innerHTML = ano
 
-  let primeiroDia = new Date(ano, mes, 1).getDay() -1
-  let ultimoDia = new Date(ano, mes + 1, 0).getDate()
-
   console.log(primeiroDia);
 
   for(i = -primeiroDia, index = 0; i < (42-primeiroDia); i++, index++){
     let dt = new Date(ano, mes, i)
-    let diaCalendario = dias.getElementsByTagName('div')[index]
+    var diaCalendario = divsDia[index]
     diaCalendario.classList.remove('outroMes')
     diaCalendario.innerHTML = dt.getDate()
 
@@ -28,6 +27,12 @@ function colocarDias(mes, ano) {
 let agora = new Date()
 let mes = agora.getMonth()
 let ano = agora.getFullYear()
+let hoje = agora.getDate()
+
+let primeiroDia = new Date(ano, mes, 1).getDay() -1
+let ultimoDia = new Date(ano, mes + 1, 0).getDate()
+
+console.log(divsDia[hoje+ primeiroDia]);
 
 colocarDias(mes, ano)
 
@@ -51,3 +56,12 @@ botao_ante.onclick = function(){
   }
   colocarDias(mes, ano)
 }
+
+const cores = document.querySelectorAll('.checkmark')
+
+cores.forEach(cor => cor.addEventListener ('click', () => {
+  var color = window.getComputedStyle(cor)
+  if(divsDia[hoje + primeiroDia].style.backgroundColor == ''){
+    divsDia[hoje + primeiroDia].style.backgroundColor = color.backgroundColor
+  }
+}))
