dc56190000d50abfd3c3219c8b809b4a90031d2f
Kayky <109158144+Deskkks@users.noreply.github.com>
2022-10-25 12:53:39 -0300

    funções organizadas

--- scpripts/main.js
+++ scpripts/main.js
@@ -3,10 +3,8 @@ const dias = document.querySelector('#dias')
 const contDias = document.querySelector('div#calendario')
 const contano = document.querySelector('div#displayAno')
 const contmes = document.querySelector('div#displayMes')
-
-const divsDia = dias.getElementsByTagName('div')
-const divsMes = contmes.getElementsByTagName('div')
-const divsAno = contano.getElementsByTagName('div') 
+var divsDia = dias.getElementsByTagName('div')
+var agora = new Date
 
 const botao_ante = document.querySelector('#btn_prev')
 const botao_prox = document.querySelector('#btn_prox')
@@ -15,165 +13,220 @@ const btn_mes = document.querySelector('span#mes')
 const btn_ano = document.querySelector('span#ano')
 
 const cores = document.querySelectorAll('.checkmark')
+var primeiroDia
 
-let agora = new Date()
-let mes = agora.getMonth()
-let ano = agora.getFullYear()
-let hoje = agora.getDate()
-var primeiroDia = new Date(ano, mes, 1).getDay() -1
-var divHoje = divsDia[hoje + primeiroDia]
-const mesA = agora.getMonth()
+let display = {}
 
-window.addEventListener('DOMContentLoaded', () => {
-  colocarDias(mes, ano)
-})
+const disCalendario = {
+  mes : agora.getMonth(),
+  ano : agora.getFullYear(),
+  hoje : agora.getDate(),
 
-function colocarDias(mes, ano) {
-  document.querySelector('span#mes').innerHTML = mesBr[mes]
-  document.querySelector('span#ano').innerHTML = ano
+  colocar(mes, ano) {
+    document.querySelector('span#mes').innerHTML = mesBr[mes]
+    document.querySelector('span#ano').innerHTML = ano
 
-  primeiroDia = new Date(ano, mes, 1).getDay() -1
-  let ultimoDia = new Date(ano, mes + 1, 0).getDate()
+    let ultimoDia = new Date(ano, mes + 1, 0).getDate()
+    primeiroDia = new Date(ano, mes, 1).getDay() -1
+    console.log(ano, mes);
 
-  for(i = -primeiroDia, index = 0; i < (42-primeiroDia); i++, index++){
-    let dt = new Date(ano, mes, i)
-    var diaCalendario = divsDia[index]
-    diaCalendario.classList.remove('outroMes')
-    diaCalendario.innerHTML = dt.getDate()
+    for(i = -primeiroDia, index = 0; i < (42-primeiroDia); i++, index++){
+      let dt = new Date(ano, mes, i)
+      var diaCalendario = divsDia[index]
+      diaCalendario.classList.remove('outroMes')
+      diaCalendario.innerHTML = dt.getDate()
 
-    if(i < 1){
-      diaCalendario.classList.add('outroMes')
+      if(i < 1){
+        diaCalendario.classList.add('outroMes')
+      }
+      if(i > ultimoDia){
+        diaCalendario.classList.add('outroMes')
+      }
     }
-    if(i > ultimoDia){
-      diaCalendario.classList.add('outroMes')
+  },
+  clickProx(){
+    disCalendario.mes++
+    if(disCalendario.mes > 11){
+      disCalendario.mes = 0
+      disCalendario.ano++
     }
-  }
-}
-
-function colocarMes() {
-  for(i = 0; i < 12; i++){
-    divsMes[i].innerHTML = mesBr[i]
-  }
-}
-
-function colocarAno() {
-  for(i = 0; i < 12; i++){
-    let anoD = ano
-    divsAno[i].innerHTML = anoD
-    anoD++
-  }
-}
-
-botao_prox.onclick = function(){
-  mes++
-  if(mes > 11){
-    mes = 0
-    ano++
-  }
-  colocarDias(mes,ano)
-}
-
-botao_ante.onclick = function(){
-  mes--
-  if(mes < 0){
-    mes = 11
-    ano--
-  }
-  colocarDias(mes, ano)
-}
-
-btn_mes.onclick = function() {
-
-  if(contDias.style.display == 'none'){
-    contDias.style.display = 'block'
-    contmes.style.display = 'none'
-    btn_ano.innerHTML = ano
-    btn_mes.innerHTML = mesBr[mes]
-  } else {
+    display.colocar(disCalendario.mes, disCalendario.ano)
+  },
+  clickAnte(){
+    disCalendario.mes--
+    if(disCalendario.mes < 0){
+      disCalendario.mes = 11
+      disCalendario.ano--
+    }
+    display.colocar(disCalendario.mes, disCalendario.ano)
+  },
+  clickMes() {
     contDias.style.display = 'none'
     contmes.style.display = 'grid'
-    btn_ano.innerHTML = ''
     btn_mes.innerHTML = 'Meses'
-    colocarMes()
-  }
-
-}
-
-btn_ano.onclick = function() {
-
-  if(contDias.style.display == 'none'){
-    contDias.style.display = 'block'
-    contano.style.display = 'none'
-    btn_ano.innerHTML = ano
-    btn_mes.innerHTML = mesBr[mes]
-  } else {
+    mudarTela(disMes)
+    display.colocar()
+  },
+  clickAno() {
     contDias.style.display = 'none'
     contano.style.display = 'grid'
     btn_ano.innerHTML = 'Anos'
     btn_mes.innerHTML = ''
-    colocarAno()
+    mudarTela(disAno)
+    display.colocar()
   }
-  
 }
 
-cores.forEach(cor => cor.addEventListener ('click', () => { 
-
-  mudarOpcaidadeSpan(cor)
-  mudarCorHoje(cor)
-}))
-
-cores.forEach(cor => cor.addEventListener('mouseenter', () => {
-
-  if(cor.style.opacity == '0.5'){
-    cor.style.opacity = '0.7'
+const disMes = {
+  colocar() {
+    var divsMes = contmes.getElementsByTagName('div')
+    for(i = 0; i < 12; i++){
+      divsMes[i].innerHTML = mesBr[i]
+    }
+  },
+  clickProx(){
+    disCalendario.ano++
+    btn_ano.innerHTML = disCalendario.ano
+  },
+  clickAnte(){
+    disCalendario.ano--
+    btn_ano.innerHTML = disCalendario.ano
+  },
+  clickMes(){
+    contDias.style.display = 'block'
+    contmes.style.display = 'none'
+    btn_ano.innerHTML = disCalendario.ano
+    btn_mes.innerHTML = mesBr[disCalendario.mes]
+    mudarTela(disCalendario)
+    display.colocar(disCalendario.mes, disCalendario.ano) 
   }
-
-}))
-
-cores.forEach(cor => cor.addEventListener('mouseout', () => {
-
-  if(cor.style.opacity == '0.7'){
-    cor.style.opacity = '0.5'
-  }
-
-}))
-
-function mudarOpcaidadeSpan(cor){
-
-  if(cor.style.opacity == '1'){
-    cor.style.opacity = '0.5'
-  }else {
-    cores.forEach(cor => cor.style.opacity = '0.5')
-    cor.style.opacity = '1'
-  }
-  
 }
 
-function mudarCorHoje(cor){
-  var color = window.getComputedStyle(cor)
+const disAno = {
+  colocar() {
+    console.log(display);
+  var divsAno = contano.getElementsByTagName('div')
+    for(i = 0; i < 12; i++){
+      divsAno[i].innerHTML = anoD
+      anoD++
+    }
+  },
+  clickProx(){
+    var divsAno = contano.getElementsByTagName('div')
+    for(i = 0; i < 12; i++){
+      divsAno[i].innerHTML = anoD
+      anoD++
+    }
+  },
+  clickAnte(){
+    anoD -= 24
+    var divsAno = contano.getElementsByTagName('div')
+    for(i = 0; i < 12; i++){
+      divsAno[i].innerHTML = anoD
+      anoD++
+    }
+  },
+  clickAno(){
+    contDias.style.display = 'block'
+    contano.style.display = 'none'
+    btn_ano.innerHTML = disCalendario.ano
+    btn_mes.innerHTML = mesBr[disCalendario.mes]
+    mudarTela(disCalendario)
+    display.colocar(disCalendario.mes, disCalendario.ano)
+  }
+}
 
-  if(mes != mesA){
-    divHoje.style.backgroundColor = 'white'
+mudarTela(disCalendario)
+
+display.colocar(disCalendario.mes, disCalendario.ano)
+let anoD = disCalendario.ano
+
+
+function mudarTela(novaTela){
+  display = novaTela
+}
+
+botao_prox.addEventListener('click', () => {
+  display.clickProx()
+})
+
+botao_ante.addEventListener('click', () => {
+  display.clickAnte()
+})
+
+btn_mes.addEventListener('click', () => {
+  display.clickMes()
+}) 
+
+btn_ano.addEventListener('click', () => {
+  display.clickAno()
+}) 
+
+const funCor = {
+  mudarCorHoje(cor){
+    var color = window.getComputedStyle(cor)
+    var mesA = agora.getMonth() 
+    var divHoje = divsDia[disCalendario.hoje + primeiroDia]
+    
+  
+    if(disCalendario.mes != mesA){
+      divHoje.style.backgroundColor = 'white'
       divHoje.style.color = 'black'
       divHoje.style.opacity = '1'
     }else{
       if(cor.style.opacity == '0.5') {
       divHoje.style.backgroundColor = 'white'
       divHoje.style.color = 'black'
-    } else {
-      
-      divHoje.style.backgroundColor = color.backgroundColor
-      divHoje.style.opacity = '0.7'
-
-      if (
-        divHoje.style.backgroundColor == 'rgb(85, 255, 0)' ||
-        divHoje.style.backgroundColor == 'rgb(225, 255, 0)' ||
-        divHoje.style.backgroundColor == 'rgb(255, 136, 0)'
-      ) {
-        divHoje.style.color = 'black'
-      }else {
-        divHoje.style.color = 'white'
+      } else {
+        divHoje.style.backgroundColor = color.backgroundColor
+        divHoje.style.opacity = '0.7'
+          if (
+            divHoje.style.backgroundColor == 'rgb(85, 255, 0)' ||
+            divHoje.style.backgroundColor == 'rgb(225, 255, 0)' ||
+            divHoje.style.backgroundColor == 'rgb(255, 136, 0)'
+            ) {
+            divHoje.style.color = 'black'
+          }else {
+            divHoje.style.color = 'white'
+          }
       }
+    }
+  },
+  spanClick(cor) {
+    if(cor.style.opacity == '1'){
+      cor.style.opacity = '0.5'
+    } else{
+      cores.forEach(cor => cor.style.opacity = '0.5')
+      cor.style.opacity = '1'
+    }
+  },
+  spanEnter(cor) {
+
+    if(cor.style.opacity == '0.5'){
+      cor.style.opacity = '0.7'
+    }
+  },
+  spanOut(cor) {
+    if(cor.style.opacity === '0.7'){
+      cor.style.opacity = '0.5'
     }
   }
 }
+
+cores.forEach(cor => cor.style.opacity = '0.5')
+
+cores.forEach(cor => cor.addEventListener ('click', () => { 
+
+  funCor.spanClick(cor)
+  funCor.mudarCorHoje(cor)
+}))
+
+cores.forEach(cor => cor.addEventListener('mouseenter', () => {
+
+  funCor.spanEnter(cor)
+}))
+
+cores.forEach(cor => cor.addEventListener('mouseout', () => {
+
+  funCor.spanOut(cor)
+}))
